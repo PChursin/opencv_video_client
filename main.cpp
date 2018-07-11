92be57b5c4ed928a6c67e64f3923dc48b8032a72
Pavel Chursin <pavelchursin@yandex.ru>
2018-07-11 23:24:39 +0400

    added sync. using fstream

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define FIFO_NAME "/tmp/opencv_video"
+#include <fstream>
+#include <thread>
+
+#define FIFO_NAME "/tmp/mkfifo_opencv"
 
 void readAll(int fdPipe, char * buf, int len)
 {
@@ -40,46 +43,71 @@ int main() {
         return 1;
     }*/
 
-    int fdPipe;
+    /*int fdPipe;
     if ((fdPipe = open(FIFO_NAME, O_RDWR)) <= 0)
     //if ((fdPipe = open(FIFO_NAME, O_RDONLY)) <= 0)
     {
         perror("open");
         return 1;
+    }*/
+    std::fstream pipe;//(FIFO_NAME, std::ios::in | std::ios::out | std::ios::binary);
+    while(!pipe.is_open())
+    {
+        pipe.open(FIFO_NAME, std::ios::in | std::ios::out | std::ios::binary);
     }
+
 
     //send sync
-    sendAll(fdPipe, "S", 1);
+    char * forSync = new char[2];
 
-    while(true)
-    {
-        int rows = 0;
-        int cols = 0;
-        int type = 0;
-        int dataSize = 0;
-        char * buf = (char*)&rows;
-        readAll(fdPipe, buf, sizeof(int));
-        buf = (char*)&cols;
-        readAll(fdPipe, buf, sizeof(int));
-        buf = (char*)&type;
-        readAll(fdPipe, buf, sizeof(int));
-        buf = (char*)&dataSize;
-        readAll(fdPipe, buf, sizeof(int));
-        buf = new char[dataSize];
-        readAll(fdPipe, buf, dataSize);
+    do {
+        strcpy(forSync, "C");
+        pipe << forSync;
+        pipe.flush();
+        std::cout << "sent" << std::endl;
+        sleep(1);
+    } while (pipe.readsome(forSync, 1) < 1 || forSync[0] == 'C');
 
-        cv::Mat tmpMat(rows, cols, type, buf);
-        if (tmpMat.empty())
+
+//    return 0;
+
+//    sendAll(fdPipe, forSync, strlen(forSync));
+
+        while(true)
         {
-            delete[] buf;
-            break;
-        }
-        cv::imshow("Video", tmpMat);
-        char c=(char)cv::waitKey(25);
-        if(c==27)
-            break;
-        delete [] buf;
-    }
+            int rows = 0;
+            int cols = 0;
+            int type = 0;
+            int dataSize = 0;
+            char * buf;// = (char*)&rows;
+//        readAll(fdPipe, buf, sizeof(int));
+            pipe.read((char*)&rows, sizeof(int));
+//        buf = (char*)&cols;
+//        readAll(fdPipe, buf, sizeof(int));
+            pipe.read((char*)&cols, sizeof(int));
+//        buf = (char*)&type;
+//        readAll(fdPipe, buf, sizeof(int));
+            pipe.read((char*)&type, sizeof(int));
+//        buf = (char*)&dataSize;
+//        readAll(fdPipe, buf, sizeof(int));
+            pipe.read((char*)&dataSize, sizeof(int));
+            buf = new char[dataSize];
+//        readAll(fdPipe, buf, dataSize);
+            pipe.read(buf, dataSize);
 
-    return 0;
+            cv::Mat tmpMat(rows, cols, type, buf);
+            if (tmpMat.empty())
+            {
+                delete[] buf;
+                break;
+            }
+            cv::imshow("Video", tmpMat);
+            char c=(char)cv::waitKey(25);
+            if(c==27)
+                break;
+            delete [] buf;
+        }
+    //read answer
+    remove(FIFO_NAME);
+    return 0;//*/
 }
